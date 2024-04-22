6605d48db54dc6b4d5a9633b24a8a31b7e2eec96
Cluyverth W. <ContatoCluyverth@gmail.com>
2024-04-22 00:03:53 -0300

    <improve>[Changes The Movement and jump of The Player]:
    [Moving the Input read to "update" and the Movement and Jumping logic to "FixedUpdate"]

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -6,8 +6,11 @@ using UnityEngine.UI;
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody2D rb;
-    private BoxCollider2D boxCollider;
+    private BoxCollider2D boxCollider2D;
     private Animator animator;
+
+    private bool jump = false;
+    private float directionMoveInput;
 
     [Header("Player Movement")]
     [SerializeField] private float moveSpeed = 5;
@@ -18,34 +21,33 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        boxCollider = GetComponent<BoxCollider2D>();
+        boxCollider2D = GetComponent<BoxCollider2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        MovePlayer();
-        if (Input.GetKeyDown(KeyCode.Space) && isGrounded()) { JumpPlayer(); }
+        directionMoveInput = Input.GetAxis("Horizontal");
+        if ((Input.GetKeyDown(KeyCode.Space) || Input.GetButtonDown("Jump")) && isGrounded()) { jump = true; }
     }
 
     private void FixedUpdate()
     {
-
+        MovePlayer();
+        if (jump) { JumpPlayer(); }
     }
 
-    private void MovePlayer()
-    {
-        float direction = Input.GetAxis("Horizontal");
+    private void MovePlayer() { rb.velocity = new Vector2(directionMoveInput * moveSpeed, rb.velocity.y); }
 
-        rb.velocity = new Vector2(direction * moveSpeed, rb.velocity.y);
-
+    private void JumpPlayer() 
+    { 
+        rb.velocity = Vector2.up * jumpForce; 
+        jump = false;
     }
-
-    private void JumpPlayer() { rb.velocity = Vector2.up * jumpForce; }
 
     private bool isGrounded()
     {
-        RaycastHit2D ground = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0, Vector2.down, 0.1f, layerGround);
+        RaycastHit2D ground = Physics2D.BoxCast(boxCollider2D.bounds.center, boxCollider2D.bounds.size, 0, Vector2.down, 0.1f, layerGround);
 
         return ground.collider != null;
     }
